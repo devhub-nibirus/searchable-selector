5985ff2bbd3cf508db9d60fee782b1a66744ae3e
Frank Vasquez <fvasquez@yawi.ai>
2026-09-08 14:33:08 -0500

    Prepare version 1.0.0 for Maven Central

--- searchable-selector/build.gradle.kts
+++ searchable-selector/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("com.vanniktech.maven.publish") version "0.37.0"
 }
 
 android {
@@ -15,7 +16,7 @@ android {
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
 
-        consumerProguardFiles("consumer-rules.pro")
+        //consumerProguardFiles("consumer-rules.pro")
     }
 
     // Actívalo después de renombrar todos los recursos.
@@ -40,4 +41,59 @@ dependencies {
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates(
+        groupId = "io.github.devhub-nibirus",
+        artifactId = "searchable-selector",
+        version = "1.0.0"
+    )
+
+    pom {
+        name.set("SearchableSelector")
+        description.set(
+            "A searchable selector for Android with single " +
+                    "and multiple selection support."
+        )
+        inceptionYear.set("2026")
+        url.set(
+            "https://github.com/devhub-nibirus/searchable-selector"
+        )
+
+        licenses {
+            license {
+                name.set("Apache License 2.0")
+                url.set(
+                    "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                )
+                distribution.set("repo")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("devhub-nibirus")
+                name.set("Frank Vasquez")
+                url.set("https://github.com/devhub-nibirus")
+            }
+        }
+
+        scm {
+            url.set(
+                "https://github.com/devhub-nibirus/searchable-selector"
+            )
+            connection.set(
+                "scm:git:https://github.com/" +
+                        "devhub-nibirus/searchable-selector.git"
+            )
+            developerConnection.set(
+                "scm:git:ssh://git@github.com/" +
+                        "devhub-nibirus/searchable-selector.git"
+            )
+        }
+    }
 }
